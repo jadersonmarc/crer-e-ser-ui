ae96ae41e08146b13ea5b55a283b93c4d6c022a6
marc jaderson <jadersonmarc@gmail.com>
2024-04-18 17:17:56 -0300

    fix large text

--- src/components/molecules/card.tsx
+++ src/components/molecules/card.tsx
@@ -24,7 +24,7 @@ function Card({
           {(title || description) && (
             <>
               {title && <p style={{ fontFamily: "Libre Baskerville" }} className="text-lg h-20 mb-4 text-center font-semibold mb-1">{title}</p>}
-              {description && <p className="text-sm font-mono text-slate-600 text-center">{description}</p>}
+              {description && <p className="text-sm font-mono text-slate-600 text-center px-5">{description}</p>}
             </>
           )}
         </div>
